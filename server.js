9d765380762104ada89f6dd0206d7b745c717110
Mrunalee Patel <mrunaleepatel997@gmail.com>
2023-08-07 14:04:51 -0500

    backend commit

---
+++ server.js
@@ -0,0 +1,87 @@
+require('dotenv').config();
+const {PORT = 3000, DATABASE_URL} = process.env;
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+// IMPORT MIDDLEWARE
+const cors = require('cors');
+const morgan = require('morgan');
+
+// DATABASE CONNECTION
+mongoose.connect(DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+// CONNECTION EVENTS
+mongoose.connection
+.on("open", () => console.log("MongoDB database connection established"))
+.on("error", (err) => console.log(err))
+.on("close", () => console.log("MongoDB database connection closed"));
+
+// MODELS 
+const CheeseSchema = new mongoose.Schema({
+    name: String,
+    countryOfOrigin: String,
+    image: String
+});
+
+const Cheese = mongoose.model('Cheese', CheeseSchema);
+
+// MIDDLEWARE
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+// ROUTES
+// test route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// CHEESE INDEX ROUTE
+app.get("/cheese", async (req, res) => {
+    try {
+        res.json(await Cheese.find({}));
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// CHEESE CREATE ROUTE
+app.post("/cheese", async (req, res) => {
+    try {
+    res.json(await Cheese.create(req.body));
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// CHEESE UPDATE ROUTE
+app.put("/cheese/:id", async (req, res) => {
+    try {
+        res.json(
+            await Cheese.findByIdAndUpdate(
+                req.params.id,
+                req.body,
+                { new: true }
+            )
+        );
+        } catch (err) {
+        res.status(500).json(err);
+    }
+        
+    });
+
+// CHEESE DELETE ROUTE
+app.delete("/cheese/:id", async (req, res) => {
+    try {
+        res.json(await Cheese.findByIdAndRemove(req.params.id));
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// LISTENER
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
